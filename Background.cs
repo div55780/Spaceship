fe460f3da1d2e51c73c8d6fba19ba32d6269778e
Alexey Semenov <smokenmoon@gmail.com>
2020-05-11 23:23:34 +0300

    Звезды и экспериментальный lock

---
+++ Background.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Spaseship
+{
+    class Background
+    {
+        public List<Star> stars = new List<Star>();
+        public Screen screen;
+
+        public void Update()
+        {
+            Random rnd = new Random();
+
+            for (int i = 1; i < 50; i++)
+            {
+                int x = rnd.Next(0, screen.width);
+                int y = rnd.Next(0, screen.height);
+                Star s = new Star();
+                s.x = x;
+                s.y = y;
+                stars.Add(s);
+            }
+        }
+
+    }
+}
--- Game.cs
+++ Game.cs
@@ -10,21 +10,22 @@ namespace Spaseship
     {
         public Screen screen;
         public Spaceship spaceship;
-        public Star star;
+        public object locker;
+        public Background bg;
+
         public void Init()
         {
             screen = new Screen();
             spaceship = new Spaceship();
             spaceship.SetPosition(screen.width / 2, screen.height);
-
-            star = new Star();
-            star.SetPosition(screen.width / 2, screen.height / 2);
+            bg = new Background();
+            bg.screen = screen;
         }
         public void Start()
         {
             spaceship.Draw();
-            star.Draw();
-
+            bg.Update();
+            locker = new object();
             Task[] tasks = new Task[2];
 
             Task shipTask = Task.Factory.StartNew(() =>
@@ -32,29 +33,42 @@ namespace Spaseship
                 InputListener shipListener = new InputListener(spaceship);
                 while (shipListener.Listen()) 
                 {
-                    spaceship.Redraw(spaceship.x, spaceship.y);
+                    lock (locker)
+                    {
+                        spaceship.Redraw(spaceship.x, spaceship.y);
+                    }
+                    
                 }
             });
             
             Task backgroundTask = Task.Factory.StartNew(async () =>
             {
+                Random rnd = new Random();
                 while (true)
                 {
-                    int newX = star.x; 
-                    int newY = star.y + 1;
-                    if (newX == spaceship.x &&  newY == spaceship.y)
+                    foreach (Star star in bg.stars)
                     {
-                        newY += 1;
+                        int newX = star.x;
+                        int newY = star.y + 1;
+                        if (newX == spaceship.x && newY == spaceship.y)
+                        {
+                            newY += 1;
+                        }
+                        
+                        if (star.y == screen.height)
+                        {
+                            newX = rnd.Next(bg.screen.width);
+                            newY = 2;
+                        }
+                        lock (locker)
+                        {
+                            star.Redraw(newX, newY);
+                        }
+                        
                     }
-                    await Task.Delay(250);
-                    if (star.y == screen.height)
-                    {
-                        newY = 1;
-                    }
-                    star.Redraw(newX, newY);
+                    await Task.Delay(150);
                 }
             });
-
             shipTask.Wait();
         }
     }
